81bfaf7845a22792bad0fc49b1919114423b3e7f
Madhan7708 <bashamadhan002@gmail.com>
2024-11-10 13:35:19 +0530

    gates changes

--- gate.php
+++ gate.php
@@ -75,7 +75,7 @@ $result = mysqli_query($conn, $query);
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="p-t-30" style="background:#25476a;">
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.php" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="gate.php" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Gate Approval</span></a></li>
 
 
                     </ul>
