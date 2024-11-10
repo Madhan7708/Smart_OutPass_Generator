e11a3d8d33d540a31b870fadb38921d9d607320c
Madhan7708 <bashamadhan002@gmail.com>
2024-11-10 12:12:23 +0530

    Db Added

---
+++ outpass (1).sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2024 at 07:45 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `outpass`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `faculty_name` varchar(255) NOT NULL,
+  `dept` varchar(255) NOT NULL,
+  `year` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gate`
+--
+
+CREATE TABLE `gate` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `security_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hod`
+--
+
+CREATE TABLE `hod` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hod_name` varchar(255) NOT NULL,
+  `dept` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaveapply`
+--
+
+CREATE TABLE `leaveapply` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `reason` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leaveapply`
+--
+
+INSERT INTO `leaveapply` (`id`, `user_id`, `category`, `reason`, `date`, `status`) VALUES
+(15, 0, 'Emergency Pass', '', '2024-11-08 19:51:00', 5),
+(16, 0, 'Emergency Pass', 'wertyui', '2024-11-08 19:53:00', 2),
+(17, 0, 'Emergency Pass', 'completetion of exam', '2024-11-09 12:38:00', 1),
+(18, 0, 'General Pass', 'aghj', '2024-11-09 10:06:00', 1),
+(19, 0, 'Emergency Pass', 'qwerrt', '2024-11-09 10:59:00', 1),
+(20, 0, 'Emergency Pass', 'asd', '2024-11-09 12:15:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `user_id`, `password`) VALUES
+(1, 1111, '12345'),
+(2, 2222, '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `mentorname` varchar(255) NOT NULL,
+  `mphoneno` varchar(10) NOT NULL,
+  `imgtext` varchar(255) NOT NULL,
+  `Dob` date NOT NULL,
+  `Year` varchar(255) NOT NULL,
+  `Dept` varchar(255) NOT NULL,
+  `Fname` varchar(255) NOT NULL,
+  `smobileno` varchar(10) NOT NULL,
+  `fmobileno` varchar(10) NOT NULL,
+  `place` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `Name`, `user_id`, `mentorname`, `mphoneno`, `imgtext`, `Dob`, `Year`, `Dept`, `Fname`, `smobileno`, `fmobileno`, `place`) VALUES
+(1, 'Madhan M', '1111', 'Durai Murugan', '7826852656', '', '2004-11-07', 'III', 'CSBS', 'Mohan raj', '7826852656', '7708482134', 'Karur'),
+(2, 'Sakthi T S', '2222', 'Karthik K', '8778475247', '', '2005-10-10', 'III', 'CSE', 'YYY', '8778952656', '9952572566', 'Karur');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gate`
+--
+ALTER TABLE `gate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hod`
+--
+ALTER TABLE `hod`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `leaveapply`
+--
+ALTER TABLE `leaveapply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gate`
+--
+ALTER TABLE `gate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hod`
+--
+ALTER TABLE `hod`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `leaveapply`
+--
+ALTER TABLE `leaveapply`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
