64eeb5a9e8511581a207b190c2ca09b98d9175bd
Madhan7708 <bashamadhan002@gmail.com>
2024-11-10 13:28:04 +0530

    apply with backend

--- back.php
+++ back.php
@@ -1,6 +1,34 @@
 <?php
 include("db.php");
 
+//raise outpass apply 
+if (isset($_POST['save_newuser'])) {
+    try {
+        $user_id = mysqli_real_escape_string($conn, $_POST['userid']);
+        $outPassType = mysqli_real_escape_string($conn, $_POST['outPass']);
+        $dateTime = mysqli_real_escape_string($conn, $_POST['time']);
+        $reason = mysqli_real_escape_string($conn, $_POST['reason']);
+
+        $query = "INSERT INTO leaveapply (user_id, category, date, reason) VALUES ('$user_id', '$outPassType', '$dateTime', '$reason')";
+
+        if (mysqli_query($conn, $query)) {
+            $res = [
+                'status' => 200,
+                'message' => 'Details added successfully'
+            ];
+            echo json_encode($res);
+        } else {
+            throw new Exception('Query Failed: ' . mysqli_error($conn));
+        }
+    } catch (Exception $e) {
+        $res = [
+            'status' => 500,
+            'message' => 'Error: ' . $e->getMessage()
+        ];
+        echo json_encode($res);
+    }
+}
+
 //class advisior approval
 if (isset($_POST['approve_user'])) {
     $apid = mysqli_real_escape_string($conn, $_POST['ids']);
