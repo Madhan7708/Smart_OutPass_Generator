511a35302a2a383145fa7d66b25d1aa9c0d14b74
Madhan7708 <bashamadhan002@gmail.com>
2024-11-10 12:11:36 +0530

    Gate  part  added

--- back.php
+++ back.php
@@ -1,34 +1,5 @@
 <?php
 include("db.php");
-//pass apply
-
-if (isset($_POST['save_newuser'])) {
-    try {
-        $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-
-        $outPassType = mysqli_real_escape_string($conn, $_POST['outPass']);
-        $dateTime = mysqli_real_escape_string($conn, $_POST['time']);
-        $reason = mysqli_real_escape_string($conn, $_POST['reason']);
-
-        $query = "INSERT INTO leaveapply (user_id, category, date, reason) VALUES ('$user_id', '$outPassType', '$dateTime', '$reason')";
-
-        if (mysqli_query($conn, $query)) {
-            $res = [
-                'status' => 200,
-                'message' => 'Details added successfully'
-            ];
-            echo json_encode($res);
-        } else {
-            throw new Exception('Query Failed: ' . mysqli_error($conn));
-        }
-    } catch (Exception $e) {
-        $res = [
-            'status' => 500,
-            'message' => 'Error: ' . $e->getMessage()
-        ];
-        echo json_encode($res);
-    }
-}
 
 //class advisior approval
 if (isset($_POST['approve_user'])) {
@@ -87,10 +58,28 @@ if (isset($_POST['approve'])) {
     }
 }
 
-//od rejected tab
+//Hod Rejected
 if (isset($_POST['reject'])) {
     $apid = mysqli_real_escape_string($conn, $_POST['ids']);
     $sql = "UPDATE leaveapply SET status ='6' WHERE id='$apid'";
+    $res = mysqli_query($conn, $sql);
+    if ($res) {
+        mysqli_commit($conn);
+        echo json_encode(['status' => 200]);
+    }
+    else {
+        $res = [
+            'status' => 500,
+            'message' => 'Details Not Deleted'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+if (isset($_POST['update_status'])) {
+    $apid = mysqli_real_escape_string($conn, $_POST['ids']);
+    $sql = "UPDATE leaveapply SET status ='4' WHERE id='$apid'";
     $res = mysqli_query($conn, $sql);
     if ($res) {
         mysqli_commit($conn);
